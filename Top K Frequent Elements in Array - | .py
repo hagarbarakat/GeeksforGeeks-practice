e167048b9932dfedeff0b91321530f3db16253a2
Hagar Barakat <hagarbarakat97@gmail.com>
2021-12-28 17:05:54 +0200

    Updated Top K Frequent Elements in Array - |

--- Top K Frequent Elements in Array - | .py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-
-class Solution:
-    def topK(self, nums, k):
-        hashmap = Counter(nums)
-    arr = sorted(hashmap.items(), key=lambda x: (x[1], x[0]), reverse=True)
-    return [x[0] for x in arr[:k]]
